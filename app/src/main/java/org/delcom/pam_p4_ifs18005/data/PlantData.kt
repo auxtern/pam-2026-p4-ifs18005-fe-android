48d49a771a43bc33a68e351eb057adccdf67d67a
Abdullah Ubaid <Abdullahfak.au@gmail.com>
2026-02-16 18:04:31 +0700

    Initial commit

---
+++ app/src/main/java/org/delcom/pam_p4_ifs18005/data/PlantData.kt
@@ -0,0 +1,9 @@
+package org.delcom.pam_p4_ifs18005.data
+
+data class PlantData(
+    val nama: String,
+    val gambar: Int,
+    val deskripsi: String,
+    val manfaat: String,
+    val efekSamping: String
+)
